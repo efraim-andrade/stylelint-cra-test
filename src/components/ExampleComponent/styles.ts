133f66033350180dd51d4586e0e115df0c14f047
Efraim Andrade <efraim.dev@gmail.com>
2020-03-28 16:12:27 -0300

    feat: 🎸 stylelint
    added the stylelint working with css files

--- src/components/ExampleComponent/styles.ts
+++ src/components/ExampleComponent/styles.ts
@@ -1,17 +1,35 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const Container = styled.div`
-  width: 100vw;
+// export const Container = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   position: relative;
+
+//   background: ${(props) => (props.color ? props.color : '#45f')};
+
+//   > h1 {
+//     color: #fff;
+//     font-size: 48px;
+//   }
+// `;
+
+.a {
+  position: relative;
+
+    width: 100vw;
   height: 100vh;
 
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 
-  background: ${props => (props.color ? props.color : '#45f')};
+  background: ${(props) => (props.color ? props.color : '#45f')};
 
   > h1 {
     color: #fff;
     font-size: 48px;
   }
-`;
+}
